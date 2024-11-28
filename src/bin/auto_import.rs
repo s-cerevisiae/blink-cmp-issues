d637d4f6ad683e296a1161012846ac787cfc4f96
spore <spore2050@gmail.com>
2024-11-28 21:32:05 +0800

    add two error cases

---
+++ src/bin/auto_import.rs
@@ -0,0 +1,6 @@
+// Ｍｕｌｔｉｂｙｔｅｓｔｒｉｎｇｓ
+
+fn main() {
+    // activate completion here and accept `swap(…) (use std::mem::swap)`
+    swap
+}
---
+++ src/bin/crate_completion.rs
@@ -0,0 +1,8 @@
+// starting from
+// `use c`
+// with auto insert, select `crate::` then type `m` without selecting. Then select `main` without accepting will turn this
+use crate::main;
+// into
+// use mainm
+
+fn main() {}
